5f3478dbaea01388a92f66494139abb19929b8c0
RaimundoValter <valter.costa@ifce.edu.br>
2025-03-21 16:58:49 -0300

    Versão com impressão e criação de elementos.

--- TAD_LISTAHET/TAD_LISTAHET.C
+++ TAD_LISTAHET/TAD_LISTAHET.C
@@ -1,10 +1,25 @@
+/*LSTHET_CRIA
+Parêmetros: 
+void
+Retorna:
+ponteiro para a cabeça da lista vazia criada.
+*/
 Listahet* lsthet_cria(void){
     return NULL;
 }
 
-Listahet* lsthet_libera(Listahet* l);
+/*LSTHET_LIBERA
+Parêmetros: 
+l    = cabeça da minha lista.
+Retorna:
+NULL
+Libera a memória de toda a lista.
+*/
+Listahet* lsthet_libera(Listahet* l){
+    
+}
 
-/*LST_INSERE
+/*LSTHET_INSERE
 Parêmetros: 
 l    = cabeça da minha lista.
 id   = chave primária que identifica unicamente um objeto da lista.
@@ -24,42 +39,94 @@ Listahet* lsthet_insere(Listahet *l, int id, int tipo, void* info){
     return novo;
 }
 
+/*LSTHET_CRIA_ELEMENTO
+Parêmetros: 
+tipo = inteiro definido que identifica o tipo da estrutura a ser criada.
+
+Retorna:
+ponteiro para a estrutura do tipo específico já alocada e inidializada.
+*/
 void* lsthet_cria_elemento(int tipo){
+
     switch(tipo){
         case RET:
-            Retangulo* ptr = (Retangulo*)malloc(sizeof(Retangulo));
-            printf("Criando um RETÂNGULO:\n")
+            Retangulo* ptr1 = (Retangulo*)malloc(sizeof(Retangulo));
+            printf("Criando um RETÂNGULO:\n");
             printf("Digite a base: ");
-            scanf("%f%*c", &ptr->b);
+            scanf("%f%*c", &ptr1->b);
             printf("Digite a altura: ");
-            scanf("%f%*c", &ptr->h);
+            scanf("%f%*c", &ptr1->h);
+            return (void*)ptr1;
         break;
         case CIR:
-            Circulo* ptr = (Circulo*)malloc(sizeof(Circulo));
-            printf("Criando um CÍRCULO:\n")
+            Circulo* ptr2 = (Circulo*)malloc(sizeof(Circulo));
+            printf("Criando um CÍRCULO:\n");
             printf("Digite o raio: ");
-            scanf("%f%*c", &ptr->r);
+            scanf("%f%*c", &ptr2->r);
+            return (void*)ptr2;
         break;
         case TRI:
-            Triangulo* ptr = (Triangulo*)malloc(sizeof(Triangulo));
-            printf("Criando um TRIÂNGULO:\n")
+            Triangulo* ptr3 = (Triangulo*)malloc(sizeof(Triangulo));
+            printf("Criando um TRIÂNGULO:\n");
             printf("Digite a base: ");
-            scanf("%f%*c", &ptr->b);
+            scanf("%f%*c", &ptr3->b);
             printf("Digite a altura: ");
-            scanf("%f%*c", &ptr->h);
+            scanf("%f%*c", &ptr3->h);
+            return (void*)ptr3;
         break;
         default:
             printf("\nTipo indefinido!!!\n");
             return NULL;
     }
-
-    return (void*)ptr;
 }
 
 Listahet* lsthet_retira(Listahet* l, int id);
 
+/*LSTHET_VAZIA
+Parêmetros: 
+l    = cabeça da minha lista.
+Retorna:
+inteiro sinalizando que a lista está
+vazia (1) ou não vazia (0).
+*/
 int lsthet_vazia(Listahet* l){
     return l == NULL;
 }
 Listahet* lsthet_busca(Listahet *l, int id);
-void lsthet_imprime(Listahet *l);
+
+/*LSTHET_IMPRIME
+Parêmetros: 
+l    = cabeça da minha lista.
+Retorna:
+Void.
+Imprime na tela todos os elementos da lista.
+*/
+void lsthet_imprime(Listahet *l){
+    for( ; l != NULL; l = l->prox){
+        switch(l->tipo){
+            case RET:
+                Retangulo* ret = (Retangulo*)l->info;
+                printf("====Retângulo====\n");
+                printf("base: %.2f\n", ret->b);
+                printf("altura: %.2f\n", ret->h);
+                printf("=================\n");
+            break;
+            case TRI:
+                Triangulo* tri = (Triangulo*)l->info;
+                printf("====Triângulo====\n");
+                printf("base: %.2f\n", tri->b);
+                printf("altura: %.2f\n", tri->h);
+                printf("=================\n");
+            break;
+            case CIR:
+                Circulo* cir = (Circulo*)l->info;
+                printf("====Círculo====\n");
+                printf("raio: %.2f\n", cir->r);
+                printf("=================\n");
+            break;
+            default:
+            printf("Tipo não suportado...\n");
+        }
+    }
+
+}
--- TAD_LISTAHET_main.c
+++ TAD_LISTAHET_main.c
@@ -5,12 +5,16 @@ int main() {
     printf("Criando um lista Heterogênea de Retângulos, Círculos e Triânguloes!\n");
     Listahet* minha_lista = lsthet_cria();
 
+    minha_lista = lsthet_insere(minha_lista, 1, RET, lsthet_cria_elemento(RET));
+    minha_lista = lsthet_insere(minha_lista, 2, CIR, lsthet_cria_elemento(CIR));
+    minha_lista = lsthet_insere(minha_lista, 3, TRI, lsthet_cria_elemento(TRI));
+    
     if(lsthet_vazia(minha_lista))
         printf("Minha lista está vazia!\n");
     else{
         printf("Lista de Tipos Heterogênesos:\n");
-        printf("...");   
+        lsthet_imprime(minha_lista);   
     }
 
-      return 0;
+    return 0;
 }
