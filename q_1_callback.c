88de7815dbd59fcb8c81f0fd402c7bff7d7fe47e
RaimundoValter <valter.costa@ifce.edu.br>
2025-04-03 14:09:17 -0300

    ...

---
+++ q_1_callback.c
@@ -0,0 +1,17 @@
+
+/*EXERCÍCIOS:
+Questão 1:
+Crie uma função chamada transforma que recebe um vetor de 
+inteiros, seu tamanho e um callback que realiza uma operação 
+matemática sobre cada elemento do vetor. O vetor deve ser 
+modificado diretamente pela função. Teste transforma() com 
+diferentes callbacks, como dobrar os valores, calcular o 
+quadrado de cada número e inverter o sinal dos elementos.
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    return 0;
+}
