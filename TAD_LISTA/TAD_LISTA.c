3da6862d863a416ffe357664cb21ec89a5a818e2
RaimundoValter <valter.costa@ifce.edu.br>
2025-04-04 17:25:36 -0300

    Criação da função main.

--- TAD_LISTA/TAD_LISTA.c
+++ TAD_LISTA/TAD_LISTA.c
@@ -1,14 +1,34 @@
 /*
+Cria uma lista genérica.
+Retorno: Ponteiro para a cabeça da lista vazia.
 */
-Lista* lst_cria(void);
+Lista* lst_cria(void){
+    return NULL;
+}
 
 /*
+Verifica se uma lista está vazia.
+Retorno: resultado lógico para a pegunta: lista está vazia? 1 para sim e 0 para não.
 */
-int lst_vazia(Lista* l);
+int lst_vazia(Lista* l){
+    return l == NULL;
+}
 
 /*
+Libera uma lista genérica.
+Retorno: NULL.
 */
-Lista* lst_libera(Lista* l);
+Lista* lst_libera(Lista* l){
+    Lista* p = l;
+
+    while( p != NULL){
+        l = l->prox;
+        free(p);
+        p = l;
+    }
+
+    return p;
+}
 
 /*
 */
