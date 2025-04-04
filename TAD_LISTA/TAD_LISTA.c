c3f50be73ae5b9d99f56aaf2b47e6a7baece5d2f
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2025-04-04 16:57:56 -0300

    Implementação da lst_busca

--- TAD_LISTA/TAD_LISTA.c
+++ TAD_LISTA/TAD_LISTA.c
@@ -18,9 +18,28 @@ Lista* lst_insere(Lista *l, void* info, int (*verifica)(void*, void*));
 */
 Lista* lst_retira(Lista* l, void* info, int (*verifica)(void*, void*));
 
+
+
 /*
+Parâmetros: 
+l= cabeça da lista,
+info= elemento que vai ser buscado,
+verifica = ponteiro para a função que vau fazer a comparação se são iguais ou não;
 */
-Lista* lst_busca(Lista *l, void* info, int (*verifica)(void*, void*));
+Lista* lst_busca(Lista *l, void* info, int (*verifica)(void*, void*)){
+    Lista* p = l;
+    while (p != NULL)
+    {
+        if (verifica(p->info,info))// Vai comparar a informação inserida com as informações que existem na lista.
+        {
+            return p;// Retorna o elemento encontrado.
+        }
+        p = p->prox;
+    } 
+    return NULL;// Não encontrou o elemento.
+}
+
+
 
 /*
 */
