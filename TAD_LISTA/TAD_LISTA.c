570b5188e7a941079586ab1d4f2df40ad9ed8f12
kesscode <kessyasantana@gmail.com>
2025-04-04 21:32:16 -0300

    Implementação da função lst_imprime

--- TAD_LISTA/TAD_LISTA.c
+++ TAD_LISTA/TAD_LISTA.c
@@ -24,8 +24,11 @@ Lista* lst_busca(Lista *l, void* info, int (*verifica)(void*, void*));
 
 /*
 */
-void lst_imprime(Lista *l, void (*imprime)(void*));
+void lst_imprime(Lista *l, void (*imprime)(void*)){
+    Lista* aux;
+    for(aux=l; aux!=NULL; aux=aux->prox){
+        imprime(aux->info);
+    }
+}
 
-/*
-*/
 int lst_map(Lista* l, void (*operacao)(void*), int (*filtro)(void*));
