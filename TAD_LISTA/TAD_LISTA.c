07e61038813237326a595b5e286f998f24373725
RaimundoValter <valter.costa@ifce.edu.br>
2025-04-05 12:12:58 -0300

    TAD_LISTA pronta e fincional.

--- TAD_LISTA/TAD_LISTA.c
+++ TAD_LISTA/TAD_LISTA.c
@@ -1,6 +1,7 @@
 #include "TAD_LISTA.h"
 
 /*
+LST_CRIA
 Cria uma lista genérica.
 Retorno: Ponteiro para a cabeça da lista vazia.
 */
@@ -9,6 +10,7 @@ Lista* lst_cria(void){
 }
 
 /*
+LST_VAZIA
 Verifica se uma lista está vazia.
 Retorno: resultado lógico para a pegunta: lista está vazia? 1 para sim e 0 para não.
 */
@@ -17,6 +19,7 @@ int lst_vazia(Lista* l){
 }
 
 /*
+LST_LIBERA
 Libera uma lista genérica.
 Retorno: NULL.
 */
@@ -33,12 +36,66 @@ Lista* lst_libera(Lista* l){
 }
 
 /*
+LST_INSERE
+Insere um novo elemento na lista genérica.
+Retorno: Lista*, ponteiro para nova cabeça da lista.
  */
-Lista *lst_insere(Lista *l, void *info, int (*verifica)(void *, void *));
+Lista *lst_insere(Lista *l, void *info, int (*verifica)(void *, void *)){
+    Lista* ant = NULL;
+    Lista* p = l;
+
+    // Cria um novo elemento para a lista.
+    Lista* novo = (Lista*)malloc(sizeof(Lista));
+    // Preenche o novo elemento com a informação.
+    novo->info = info;
+
+    // Encontrando a posição onde inserir o elemento.
+    for(ant = NULL, p = l; p != NULL; ant = p, p = p->prox)
+        if(verifica(p->info, info))
+            break;
+
+    // Nesse ponto, temos apenas 4 possibilidades para ant e p:
+    // ant == NULL && p == NULL: indica que a lista está vazia e a inserção vai ocorrer na cabeça da lista.
+    // ant == NULL && p != NULL: indica que a lista não está vazia e a inserção vai ocorrer na cabeça da lista.
+    // ant != NULL && p != NULL: indica que a lista não está vazia e a inserção vai ocorrer no corpo da lista.
+    // ant != NULL && p == NULL: indica que a lista não está vazia e que a inserção ocorrerá no fim da lista.
+
+    // Caso a lista esteja vazia
+    if (ant == NULL && p == NULL){
+        // Marca o elemento como o último da lista.
+        novo->prox = NULL;
+        // Atualiza o ponteiro para a nova cabeça da lista.
+        l=novo;
+    }
+    // Caso o elemento tenha que ser inserido no início da lista.
+    else if (ant == NULL && p != NULL){
+        // Marca o elemento como o primeiro da lista.
+        novo->prox = l;
+        // Atualiza o ponteiro para a nova cabeça da lista.
+        l=novo;
+    }
+    // Caso o elemento tenha que ser inserido no corpo da lista
+    else if(ant != NULL && p != NULL){
+        // Marca o elemento como inserido no corpo da lista.
+        novo->prox = p;
+        // Atualiza o elemento anterior para apontar para o novo elemento da lista.
+        ant->prox = novo;
+    }
+    // Caso o elemento tenha que ser inserido no final da lista que não está vazia
+    else if(ant != NULL && p == NULL){
+        // Marca o elemento como fim da lista.
+        novo->prox = NULL;
+        // Atualiza o elemento anterior para apontar para o novo elemento da lista.
+        ant->prox = novo;
+
+    }
+
+    // Retorna a nova cabeça da lista.
+    return l;
+}
 
 /*
-
-LST RETIRA:
+LST RETIRA
 parametros: l (cabeca da lista).
 info: informação passada, sendo void pois não tem um tipo definido.
 callback que retorna uma boleana, retornando 1 se for pra excluir, e 0 para não excluir.
@@ -54,6 +111,7 @@ Lista* lst_retira(Lista* l, void* info, int (*verifica)(void*, void*))
 {
     Lista * p = l;
     Lista * anterior = NULL;
+
     while (p != NULL)
     {
         if(verifica(p->info,info))
@@ -65,21 +123,35 @@ Lista* lst_retira(Lista* l, void* info, int (*verifica)(void*, void*))
         p = p->prox;
     }
 
+    // Elemento não encontrado para exclusão
+    if(p == NULL)
+        return l;
+
+    // Elemento encontrado para exclusão na cabeça da lista.
     if(anterior == NULL)
     {
-        p = p->prox; 
+        // Atualiza a nova cabeça da lista, retirando o elemento apontado por p da sequência da lista.
+        l = p->prox;
     }
+    // Elemento encontrado no corpo ou no fim da lista.
     else
     {
+        // Atualiza retirado o elemento apontado por p da sequência da lista.
         anterior->prox = p->prox;
     }
+
+    // Libera a informação dentro do elemento da lista para exclusão
     free(p->info);
+    // Libera o elemento da lista para a exclusão.
     free(p);
+
+    // Retorna a cabeça da lista.
     return l;
     
 }
 
 /*
+LST_BUSCA
 Parâmetros: 
 l= cabeça da lista,
 info= elemento que vai ser buscado,
@@ -99,6 +171,7 @@ Lista* lst_busca(Lista *l, void* info, int (*verifica)(void*, void*)){
 }
 
 /*
+LST_MAP
 Função lst_map, percorre a lista "Lista *l", verificando se o elemnto "l->info" satisfaz
 a condição da função "filtro" passada por parâmetro. Se satisfeita a condição, "l-info"
 é passada como parâmetro para a função "operacao" esta que é, também, passada como parâmetro na função
@@ -125,6 +198,13 @@ int lst_map(Lista *l, void (*operacao)(void *), int (*filtro)(void *))
     return 1;
 }
 
+/*
+LST_IMPRIME
+Parâmetros:
+Lista *l - recebe um ponteiro para a estrutura "Lista" que contem os
+    campos: void* info e struct lista* prox;
+void (*imprime)(void *) - callback para uma função que manipula a informação e a imprime na tela.
+*/
 void lst_imprime(Lista *l, void (*imprime)(void*)){
     Lista* aux;
     for(aux=l; aux!=NULL; aux=aux->prox){
--- exemplo_lista_generica.c
+++ exemplo_lista_generica.c
@@ -1,61 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "./TAD_lISTA/TAD_LISTA.h"
+#include "./TAD_LISTA/TAD_LISTA.h"
 
 int verifica_posicao(void* elemento_posicao, void* elemento_inserir);
 int todos_elementos(void* elemento_posicao);
 int elementos_impares(void* elemento_posicao);
 int se_igual(void* elemento1, void* elemento2);
 void duplica(void* elemento);
+void formatacao(void* elemento);
 
 int main(int argc, char* argv[]){
     
-    int qtd_elementos=0, elemento_exclusao = 0;
-    int* elemento_busca = NULL;
-    int* elemento_encontrado = NULL;
+    int qtd_elementos=0;
 
     Lista* minha_lista = lst_cria();
 
-    printf("Nossa lista genérica!!!\nFaremos uma lista de números interios.");
+    printf("=> Nossa lista genérica!!!\n\
+        Faremos uma lista de números INTEIROS.");
 
-    printf("Digite a quantidade de elementos que quer inserir: ");
+    printf("\nDigite a quantidade de elementos\n\
+         INTEIROS que quer inserir na LISTA: ");
     scanf("%d%*c", &qtd_elementos);
     
+    // Inserindo inteiros na minha lista.
     int* novo_elemento;
 
     for(int i = 0; i < qtd_elementos; i++){
+        // Alocando a memória para acondicionar um inteiro.
         novo_elemento = (int*)malloc(sizeof(int));
-        *novo_elemento = rand()%100;
-        lst_insere(minha_lista, (void*)novo_elemento, verifica_posicao);
+        // Escolhendo um inteiro aleatoriamente no intervalo de 0 a 100
+        *novo_elemento = rand()%101;
+        // Insere o elemento na lista.
+        minha_lista = lst_insere(minha_lista, (void*)novo_elemento, verifica_posicao);
     }
 
-    lst_imprime(minha_lista, todos_elementos);
+    // Imprime a lista de inteiros criada.
+    lst_imprime(minha_lista, formatacao);
 
-    printf("\nDigite um número para excluir: ");
-    scanf("%d%*c", &elemento_exclusao);
+    // Excluir um número inteiro.
+    int* elemento_exclusao = (int*)malloc(sizeof(int));
+
+    printf("\nDigite um número INTEIRO para excluir: ");
+    scanf("%d%*c", elemento_exclusao);
 
     minha_lista = lst_retira(minha_lista, (void*)elemento_exclusao, se_igual);
     
-    lst_imprime(minha_lista, todos_elementos);
+    // Imprime minha lista após exclusão do elemento.
+
+    printf("\nMinha lista após exclusão: \n");
+    lst_imprime(minha_lista, formatacao);
+
+    //Buscar um número inteiro.
+    int* elemento_busca = (int*)malloc(sizeof(int));
+    Lista* elemento_encontrado = NULL;
 
     printf("\nDigite um número para buscar: ");
-    scanf("%d%*c", &elemento_busca);
+    scanf("%d%*c", elemento_busca);
 
     elemento_encontrado = lst_busca(minha_lista, (void*)elemento_busca, se_igual);
 
     if (elemento_encontrado)
-        printf("\nElemento %d encontrado!\n", *((int*)elemento_encontrado));
+        printf("\nElemento %d encontrado!\n", *((int*)(elemento_encontrado->info)));
     else
         printf("\nElemento não encontrado!\n");
     
-    printf("\nUtilizando a função MAP para duplicar a lista de inteiros...\n");
+    // Utilizando a função MAP para duplicar os elementos da minha lista
+    printf("\n\nUtilizando a função MAP para duplicar a lista de inteiros...\n");
 
-    if(map(minha_lista, duplica, todos_elementos))
-        lst_imprime(minha_lista, todos_elementos);
+    if(lst_map(minha_lista, duplica, todos_elementos))
+        lst_imprime(minha_lista, formatacao);
 
+    
+    // Liberando a lista completa.
+    printf("\n\nLiberando minha Lista de INTEIROS...\n");
+    
     minha_lista = lst_libera(minha_lista);
-
-    printf("\nLista de inteiros liberada...\n");
 
     if(lst_vazia(minha_lista))
         printf("\nLista liberada com sucesso...\n");
@@ -65,7 +84,7 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-// Funções úteis
+// Funções ÚTEIS
 // Retorna a verificação lógica para saber se a posição de inserção 
 // do novo elemento na lista é aceitável. Retorna 1 para aceitável ou
 // 0 para não.
@@ -73,17 +92,24 @@ int verifica_posicao(void* elemento_posicao, void* elemento_inserir){
     return (int)elemento_posicao > (int)elemento_inserir;
 }
 
+// Define o formato e imprime elemento da lista genérica.
+void formatacao(void* elemento){
+    printf("| %d |", *((int*)elemento));
+}
+
 // Retorna a verificação lógica para identificar que
 // todos os valores são para impressão.
 int todos_elementos(void* elemento){
-    prinf("| %d |", *((int*)elemento));
+    return 1;
 }
 
 // Retorna a verificação lógica para identificar
 // os elementos ímpares para impressão.
 int elementos_impares(void* elemento){
     if (*((int*)elemento) % 2 != 0)
-        prinf("| %d |", *((int*)elemento));
+        return 1;
+    else
+        return 0;
 }
 
 // Retorna a verificação lógica para identificar
