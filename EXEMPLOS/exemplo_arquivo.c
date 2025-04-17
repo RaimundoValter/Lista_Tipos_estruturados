2a4286d42affaf8d5885098e2b847e893e30e93d
RaimundoValter <valter.costa@ifce.edu.br>
2025-04-17 15:18:26 -0300

    ARQUIVOS

--- EXEMPLOS/exemplo_arquivo.c
+++ EXEMPLOS/exemplo_arquivo.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-int main(){
-    
+int main(int qtd_param, char* param[]){
+
+    if (qtd_param == 2){
+        char* nome_arquivo = param[1];
+
+        FILE* fp = fopen(nome_arquivo, "rt");
+        
+        if(!fp){
+            printf("Erro ao abrir o arquivo: %s", nome_arquivo);
+            exit(1);
+        }
+        
+        printf("\nArquivo abriu com sucesso!\n");
+
+        fclose(fp);
+    }
+    return 0;
 }
