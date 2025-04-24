10b373482610ca88bd52865f4d4c60a63a718781
RaimundoValter <valter.costa@ifce.edu.br>
2025-04-24 15:26:16 -0300

    ARQUIVOS - Parte 2

--- EXEMPLOS/exemplo_arquivo.c
+++ EXEMPLOS/exemplo_arquivo.c
@@ -11,10 +11,33 @@ int main(int qtd_param, char* param[]){
             printf("Erro ao abrir o arquivo: %s", nome_arquivo);
             exit(1);
         }
-        
+
         printf("\nArquivo abriu com sucesso!\n");
 
+        FILE* saida = fopen("saida.txt", "wt");
+
+        if(!saida){
+            printf("Erro ao abrir o arquivo: saida.txt");
+            exit(1);
+        }
+
+        int c = 0;
+        int nlinhas = 0;
+
+        while((c = fgetc(fp)) != EOF){
+            if(c == '\n')
+                nlinhas++;
+            
+            if((int)'A' <= c && c <= (int)'Z')
+                c = c - (int)'A' + (int)'a';
+
+            fputc(c, saida);
+        }
+        
+        printf("Foram processadas  %d linhas no arquivo %s...\n", nlinhas + 1, nome_arquivo);
+        
         fclose(fp);
+        fclose(saida);
     }
     return 0;
 }
