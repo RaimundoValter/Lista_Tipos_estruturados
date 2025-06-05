d320c38ddeb6dae52e35a415353d7828ca9a7ba7
RaimundoValter <valter.costa@ifce.edu.br>
2025-06-05 13:54:11 -0300

    TAD_TREE: início dos trabalhos.

--- TAD_TREE/TAD_TREE.h
+++ TAD_TREE/TAD_TREE.h
@@ -8,12 +8,19 @@
 
     int tree_empty(Node* root);
     Node* tree_create_empty();
-    Node* tree_create_node(void* info, Node* lst, Node* rst);
-    int tree_insert_node(Node* root, Node* new_node, int (compare)(void*, void*));
     void tree_free(Node* root);
-    void tree_map(Node* root, void (operation)(void*));
+ 
+    Node* tree_create_node(void* info, Node* lst, Node* rst); // Santana
+    void tree_map(Node* root, void (operation)(void*)); // Linhares
+    Node* tree_search(Node* root, int (condition)(void*)); // Damasceno
+    int tree_update(Node* root, void (update)(void*)); // Silva
+ 
     Node* tree_filter(Node* root, int (condition)(void*));
-    Node* tree_search(Node* root, int (condition)(void*));
+    Node* tree_delete_node(Node* root, int (condition)(void*));
+    Node* tree_insert_node(Node* root, Node* new_node, int (compare)(void*, void*));
+
+    Node* tree_insert_balanced(Node* root, Node* new_node, int (compare)(void*, void*)); //Ribeiro
+    Node* tree_delete_balanced(Node* root, Node* new_node, int (compare)(void*, void*)); //Medonça
 
     #include "TAD_TREE.c"
 #endif
